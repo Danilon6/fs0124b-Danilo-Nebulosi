0c262e942bcded58f77ea8ee070f0cb117f03be4
Danilon6 <danilonebulosi@gmail.com>
2024-02-08 16:15:03 +0100

    exercise_done

--- M_1/S_3/G_4/Js/script.js
+++ M_1/S_3/G_4/Js/script.js
@@ -14,3 +14,44 @@ function aggiungiCartella () {
     }
     container_esterno_cartelle.append(divInterno)
 }
+
+
+const tabellone = document.querySelector("#Tabellone")
+const iniziaPartita = document.querySelector("#inizia_partita")
+const button_estrai_numero = document.querySelector("#estrai_numero")
+iniziaPartita.addEventListener("click", creaTabellone)
+let numero_tabellone;
+let iniziaPartitaCliccato = false;
+
+function creaTabellone() {
+
+    for (let i = 1; i < 91; i++) {
+        const numero = document.createElement("div")
+        numero.classList.add("numero_tabellone")
+        numero.innerText = i
+        tabellone.append(numero)
+    }
+
+    if (!iniziaPartitaCliccato) {
+        iniziaPartita.disabled = true
+        iniziaPartitaCliccato = true
+        button_estrai_numero.style.opacity = 1
+        buttonAggiungiCartella.disabled = true
+    }
+
+    numero_tabellone = document.querySelectorAll(".numero_tabellone")
+    button_estrai_numero.addEventListener("click", estraiNumero)
+    }
+
+    function estraiNumero () {
+        let numeroEstratto = Math.round(Math.random()*89)+1
+        numero_tabellone.forEach(function (numero){
+            let numeroTabellone = parseInt(numero.innerText)
+            if ( numeroTabellone == numeroEstratto ) {
+                numero.style.backgroundColor = "red"
+            }
+        })
+            
+    }
+
+
