762ed86ce824ebb81b1fc6c2fde376f78b1302f3
Danilon6 <danilonebulosi@gmail.com>
2024-02-24 20:25:59 +0100

    fix_repsonsive_quasi_ristrutturazione_html_e_css

--- M_2/S_1/G_5_Progetto/assets/js/script.js
+++ M_2/S_1/G_5_Progetto/assets/js/script.js
@@ -1,15 +1,20 @@
 // SELEZIONE HEADER
 let header = document.querySelector("#header")
 
+// SELEZIONE BUTTON GET STARTED
+let getStartedBtn = document.querySelector("#Get_started")
+
 // ADD EVENT LISTENER SULLO SCROLL DELLA PAGINA
 window.addEventListener("scroll", function () {
     let top = window.scrollY
     console.log(top);
-    if (top>360) {
+    // CAMBIO COLORE ALL'HEADER E AL BTN
+    if (top > 360) {
         header.style.backgroundColor = "white"
-        
-    }else{
+        getStartedBtn.style.backgroundColor = "#1a8917"
+    } else {
         header.style.backgroundColor = "#FFC017"
+        getStartedBtn.style.backgroundColor = "#000"
     }
 })
 
