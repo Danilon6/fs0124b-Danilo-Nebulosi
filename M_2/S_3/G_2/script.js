5ea5a201b0cb902417d6b8f19973e3ab468e1dff
Danilon6 <danilonebulosi@gmail.com>
2024-03-05 16:38:37 +0100

    exercise_one_with_class

--- M_2/S_3/G_2/script.js
+++ M_2/S_3/G_2/script.js
@@ -1,25 +1,81 @@
 // ESERCIZIO 1
 
-{
-    let inputfield = document.querySelector("#inputName")
-    let savedDataP = document.querySelector("#savedData")
-    let saveBtn = document.querySelector("#saveBtn")
-    let deleteBtn = document.querySelector("#deleteBtn")
 
-    saveBtn.addEventListener("click", function () {
-        savedDataP.innerHTML = ""
-        localStorage.setItem("nome", JSON.stringify(inputfield.value))
+class SalvaDati {
+
+    constructor() {
+        this.target = document.querySelector("#target")
+        this.inputfield = null
+        this.savedDataP = null
+        this.saveBtn = null
+        this.deleteBtn = null
+
+        this.initHTML()
+
+        this.target.append(this.savedDataP, this.inputfield, this.saveBtn, this.deleteBtn)
+    }
+
+    initHTML() {
+        this.inputfield = this.creaElementoConId("input", "inputName")
+        this.savedDataP = this.creaElementoConId("p", "savedData")
+        this.saveBtn = this.creaElementoConId("button", "saveBtn")
+        this.saveBtn.innerText = "Salva il dato"
+        this.deleteBtn = this.creaElementoConId("button", "deleteBtn")
+        this.deleteBtn.innerText = "Elimina il dato"
+
+        this.eventListener(this.saveBtn, this.saveBtnCallback.bind(this))
+        this.eventListener(this.deleteBtn, this.deleteBtnCallback.bind(this))
+    }
+
+    creaElementoConId(tagName, id) {
+        let elemento = document.createElement(tagName)
+        elemento.setAttribute("id", id)
+        return elemento
+    }
+
+    eventListener(button, callback) {
+        button.addEventListener("click", callback)
+    }
+
+    saveBtnCallback() {
+        this.savedDataP.innerHTML = ""
+        localStorage.setItem("nome", JSON.stringify(this.inputfield.value))
         let savedData = localStorage.getItem("nome")
-        let savedDataString = localStorage = JSON.parse(savedData)
-        savedDataP.append(savedDataString)
-    })
+        let savedDataString = JSON.parse(savedData)
+        this.savedDataP.append(savedDataString)
+    }
 
-    deleteBtn.addEventListener("click", function () {
-        savedDataP.innerHTML = ""
-        inputfield.value = ""
+    deleteBtnCallback() {
+        this.savedDataP.innerHTML = ""
+        this.inputfield.value = ""
         localStorage.removeItem("nome")
-    })
+    }
+
 }
+
+let test = new SalvaDati();
+
+// INIZIO ESERCIZIO SENZA LA CLASSE
+//     let inputfield = document.querySelector("#inputName")
+//     let savedDataP = document.querySelector("#savedData")
+//     let saveBtn = document.querySelector("#saveBtn")
+//     let deleteBtn = document.querySelector("#deleteBtn")
+
+//     saveBtn.addEventListener("click", function () {
+//         savedDataP.innerHTML = ""
+//         localStorage.setItem("nome", JSON.stringify(inputfield.value))
+//         let savedData = localStorage.getItem("nome")
+//         let savedDataString = JSON.parse(savedData)
+//         savedDataP.append(savedDataString)
+//     })
+
+//     deleteBtn.addEventListener("click", function () {
+//         savedDataP.innerHTML = ""
+//         inputfield.value = ""
+//         localStorage.removeItem("nome")
+//     })
+// }
+// FINE ESERCIZIO SENZA LA CLASSE
 
 
 
