5817816700755566cfc894b68a62ff45a43fb8c4
Danilon6 <danilonebulosi@gmail.com>
2024-03-22 16:36:46 +0100

    all-done

--- M_3/S_1/G_5_Progetto/Car_Showroom/src/app/pages/home/home.component.ts
+++ M_3/S_1/G_5_Progetto/Car_Showroom/src/app/pages/home/home.component.ts
@@ -14,11 +14,26 @@ export class HomeComponent {
   randomCarsArr:iCars[] = [];
 
   allLogoArr: string[] = [];
+
+  carsIdArr:string[] =[];
+
+  idRandom:string = "";
+
   constructor(private carSvc:CarsService){}
 
   ngOnInit():void{
     this.carSvc.getAllLogo().then(loghi => this.allLogoArr = loghi)
     this.carSvc.getAllcars().then(cars => this.carsArr = cars)
     this.carSvc.getRandomAvailableCars(2).then(cars => this.randomCarsArr = cars)
+    this.carSvc.getAllcars().then(cars => {
+      for (let i = 0; i < cars.length; i++) {
+        this.carsIdArr.push(cars[i].id)
+      }
+      return this.carsIdArr
+    })
+    .then(carsIdArr =>{
+      const randomIndex =  Math.floor(Math.random() * carsIdArr.length);
+        this.idRandom = carsIdArr[randomIndex]
+    })
   }
 }
